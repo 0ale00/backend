9915c6e06357336a45e2411c1b11576b858b7e6e
0ale00 <alessiagrande@hotmail.it>
2023-01-29 19:19:33 +0100

    agg

--- lezione5/postex.php
+++ lezione5/postex.php
@@ -1,15 +1,16 @@
 <?php
-if (isset($_GET["name"])) {
-    echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+
+if (isset($_POST["name"])) {
+    echo 'Nome ' . htmlspecialchars($_POST["name"]) . '!';
 }
 
-if (isset($_GET["surname"])) {
-    echo 'Cognome' . htmlspecialchars($_GET["surname"]) . '!';
+if (isset($_POST["surname"])) {
+    echo 'Cognome ' . htmlspecialchars($_POST["surname"]) . '!';
 }
 
 ?>
-<p>Test</p>
-<form method="GET" action="ricevechiamate.php">
+<p>Anagrafica</p>
+<form method="POST" action="ricevechiamate.php">
     <input type="text" id="name" name="name" />
     <input type="text" id="surname" name="surname" />
     <input type="submit" value="Invia" />
