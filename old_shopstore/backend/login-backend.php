a6227f99fd8083183ad3d35511089c9d1a04f2a6
0ale00 <alessiagrande@hotmail.it>
2023-02-14 13:46:54 +0100

    shop

--- old_shopstore/backend/login-backend.php
+++ old_shopstore/backend/login-backend.php
@@ -1,22 +1,25 @@
 <?php
-require_once "inc/functions.php";
+require_once "../inc/functions.php";
 if (!empty($_POST["login"])) {
     session_start();
     $username = $_POST["user_name"];
     $password = $_POST["password"];
 
     $isLoggedIn = false;
-    $myAnagr = readFileJson("data/anagrafica.json");
+    $myAnagr = readFileJson("../data/users.json");
 
     foreach ($myAnagr as $key => $userArray) {
         if ($userArray['username'] == $username && $userArray['pwd'] == $password) {
             $isLoggedIn = true;
             $_SESSION["userId"] = $key;
+            $_SESSION["isAdmin"] = true;
             $_SESSION["userInfo"] = $userArray['nome'] . ' ' . $userArray['cognome'];
         }
     }
     if (!$isLoggedIn) {
         $_SESSION["errorMessage"] = "Username o password non valide!";
+        header("Location: login.php");
+    } else {
+        header("Location: ../index.php");
     }
-    header("Location: index.php");
 }
