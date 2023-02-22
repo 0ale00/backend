e52d2f2dc03b9ab046e149c8bf1c8de895224e68
0ale00 <alessiagrande@hotmail.it>
2023-02-22 14:25:31 +0100

    faker ex

---
+++ faker/database/factories/ArticleFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Article;
+use App\Models\User;
+use Faker\Generator as Faker;
+
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text,
+        'user_id' =>  User::factory()->create(),
+        'slug' => $faker->slug,
+        'keywords' => $faker->text,
+        'description' => $faker->text,
+        'content' => $faker->paragraph,
+    ];
+});
