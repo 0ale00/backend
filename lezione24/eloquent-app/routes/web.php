b4c8165d18f26b23e810ecd5f27a5e2adf79954a
0ale00 <alessiagrande@hotmail.it>
2023-02-27 14:26:46 +0100

    lezione25

--- lezione24/eloquent-app/routes/web.php
+++ lezione24/eloquent-app/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\UserController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,13 +30,32 @@ Route::get('userphone/{id}', [UserController::class, 'phone']);
 Route::get('phoneid/{idphone}', [UserController::class, 'getUserFromIdPhone']);
 
 use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
 use App\Models\User;
+ 
 Route::get('/user/{id}', function (string $id) {
     return new UserResource(User::findOrFail($id));
 });
 
-use App\Http\Resources\FlightResource;
-use App\Models\flight;
-Route::get('/volo/{id}', function (string $id) {
-    return new UserResource(Flight::findOrFail($id));
+Route::get('/users', function () {
+    return new UserCollection(User::all());
 });
+
+Route::get('/searchuser', function () {
+    return view('searchuser');
+});
+
+Route::post('/search', [UserController::class, 'search']);
+
+use App\Http\Resources\FlightResource;
+use App\Models\Flight;
+ 
+Route::get('/volo/{id}', function (string $id) {
+    return new FlightResource(Flight::findOrFail($id));
+});
+
+use App\Http\Controllers\SesController;
+
+Route::get('session/get', [SesController::class, 'accessSessionData']);
+Route::get('session/set', [SesController::class, 'storeSessionData']);
+Route::get('session/remove', [SesController::class, 'deleteSessionData']);
