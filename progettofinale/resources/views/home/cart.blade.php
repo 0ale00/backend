2b8ad9ae329c7bd13c004422eb8a95f38e9a1fce
0ale00 <alessiagrande@hotmail.it>
2023-02-28 14:21:06 +0100

    cart

--- progettofinale/resources/views/home/cart.blade.php
+++ progettofinale/resources/views/home/cart.blade.php
@@ -2,32 +2,18 @@
 
 @section('content')
     @auth
-        <h1>Dashboard</h1>
+        <h1>Carrello</h1>
         <p class="lead">Sei un utente autenticato con ID {{ Auth::id() }}</p>
     @endauth
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
 
-        @foreach ($products as $product)
-            <div class="col">
-                <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
-                    style="background-image: url('unsplash-photo-1.jpg');">
-                    <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                        <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{{ $product->name }}</h2>
-                        <h3>€ {{ $product->price }} </h3>
-                        <img src="{{ $product->image }}" />
-                        <ul class="d-flex list-unstyled mt-auto">
+     <?php
+     var_dump(session('cart'));
+     echo '<hr>';
 
-                            <p> {{ $product->description }} </p>
-                            <li class="d-flex align-items-center me-3">
-
-                                <small>INSERISCI NEL CARRELLO </small>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        @endforeach
+        $cart = session('cart');
+        echo  $cart['quantity']. ' '.$cart['name']. ' '.$cart['price'].'<br>';
+?>
 
     </div>
 @endsection
--- progettofinale/routes/web.php
+++ progettofinale/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -50,9 +49,7 @@ Route::resource('products', ProductController::class)->missing(function (Request
     return Redirect::route('product.index');
 });
 
+use App\Http\Controllers\CartController;
 
 Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
 Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
-Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
-Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
-Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
