2ffd5b2187b983b06729ea0e89af2213a6d6c01d
0ale00 <alessiagrande@hotmail.it>
2023-02-28 14:28:46 +0100

    progettofinale

--- progettofinale/app/Http/Controllers/CartController.php
+++ progettofinale/app/Http/Controllers/CartController.php
@@ -6,61 +6,24 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function cartList()
-    {
-        $cartItems = \Cart::getContent();
-        // dd($cartItems);
-        return view('cart', compact('cartItems'));
-    }
-
-
     public function addToCart(Request $request)
     {
-        \Cart::add([
-            'id' => $request->id,
+        //  session(['key' => 'value']);
+        session()->push('cart', ['id' => $request->id,
             'name' => $request->name,
             'price' => $request->price,
             'quantity' => $request->quantity,
-            'attributes' => array(
-                'image' => $request->image,
-            )
-        ]);
+            'image' => $request->image]);
+
         session()->flash('success', 'Product is Added to Cart Successfully !');
 
-        return redirect()->route('cart.list');
-    }
-
-    public function updateCart(Request $request)
-    {
-        \Cart::update(
-            $request->id,
-            [
-                'quantity' => [
-                    'relative' => false,
-                    'value' => $request->quantity
-                ],
-            ]
-        );
-
-        session()->flash('success', 'Item Cart is Updated Successfully !');
+        // var_dump(session('cart'));
 
         return redirect()->route('cart.list');
     }
 
-    public function removeCart(Request $request)
+    public function cartList(Request $request)
     {
-        \Cart::remove($request->id);
-        session()->flash('success', 'Item Cart Remove Successfully !');
-
-        return redirect()->route('cart.list');
-    }
-
-    public function clearAllCart()
-    {
-        \Cart::clear();
-
-        session()->flash('success', 'All Item Cart Clear Successfully !');
-
-        return redirect()->route('cart.list');
+        return view('home.cart', $request);
     }
 }
